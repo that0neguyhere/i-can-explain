37982c206dc185ce11d377f233212fc996e1c9c8
that0neguyhere <122296314+that0neguyhere@users.noreply.github.com>
2023-06-21 13:57:11 +0300

    Update and rename Nameless Neko.lua to Nameless Neeko.lua

--- Nameless Neeko.lua
+++ Nameless Neeko.lua
@@ -645,15 +645,15 @@ local function reanimate()
 
 
 	local accessorylimbs={
-		{meshid="13421774668",textureid="13415110780",C0=cf_0,Name="Torso"},
 		{meshid="5673931939",textureid="5673921683",C0=cf(0.5,0.4, -0.5)*angles(rad(-180),rad(0),rad(0)),Name="Torso"},
 		{meshid="5673931939",textureid="5673921282",C0=cf(-0.5,0.4, -0.5)*angles(rad(-180),rad(0),rad(0)),Name="Torso"},
 		{meshid="4948672433",textureid="5132781531",C0=cf(-0,0.5, -0.99)*angles(rad(-180),rad(0),rad(0)),Name="Torso"},
-		{meshid="13421705040",textureid="13415112383",C0=angles(rad(116),rad(4),rad(22)),Name="Left Arm"},
-		{meshid="13415096670",textureid="13415112383",C0=angles(rad(112),rad(-13.5),rad(-22)),Name="Right Arm"},
-		{meshid="13421798868",textureid="13415112383",C0=angles(rad(47.5),rad(40),rad(24.5)),Name="Left Leg"},
-		{meshid="13421836805",textureid="13415112383",C0=angles(rad(56),rad(-22),rad(-20.75)),Name="Right Leg"}
-	}
+        	{meshid="13421774668",textureid="13415110780",C0=cf_0,Name="Torso"},
+    		{meshid="12344207333",textureid="12344207341",C0=angles(rad(90),rad(0),rad(0)),Name="Left Arm"},
+        	{meshid="12344206657",textureid="12344206675",C0=angles(rad(90),rad(0),rad(0)),Name="Right Arm"},
+        	{meshid="11263221350",textureid="11263219250",C0=angles(rad(0),rad(0),rad(90)),Name="Left Leg"},
+        	{meshid="11159370334",textureid="11159284657",C0=angles(rad(0),rad(0),rad(90)),Name="Right Leg"}
+    }
 
 	for i=1,#accessorylimbs do
 		local v=accessorylimbs[i]
